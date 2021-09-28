82ff4dc63e4dea2e8ee504cf4c9f54ff583088e8
dorian <dorian.fenestre@free.fr>
2021-09-28 18:04:54 +0200

    first commit

---
+++ src/Controller/MainController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class MainController extends AbstractController
+{
+    /**
+     * Page d'accueil
+     * @Route ("/", name="home")
+     */
+    public function home()
+    {
+        return $this->render("main/home.html.twig");
+    }
+
+    /**
+     * @Route ("/test", name="test")
+     */
+    public function test()
+    {
+        $ideas = ["voyage", "lecture", "jeux"];
+
+        return $this->render("main/test.html.twig", [
+            "ideas" =>$ideas
+        ]);
+    }
+}
