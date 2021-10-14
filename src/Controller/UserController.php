6ce775468652bec585286956a273a943ae287190
dorian <dorian.fenestre@free.fr>
2021-10-14 17:25:40 +0200

    creation of User entity, registration and login - err Unrecognized option logout_on_user_change

---
+++ src/Controller/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/register", name="user_register")
+     */
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $hasher): Response
+    {
+
+        $user = new User();
+        $registerForm = $this->createForm(RegisterType::class, $user);
+
+        $registerForm->handleRequest($request);
+        if ($registerForm->IsSubmitted() && $registerForm->isValid()) {
+            $hashed = $hasher->hashPassword($user, $user->getPassword());
+            $user->setPassword($hashed);
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render('user/register.html.twig', [
+            'registerForm' => $registerForm->createView()
+        ]);
+    }
+
+    /**
+     * @Route ('/login', name='login')
+     */
+    public function  login()
+    {
+
+    return $this->render('user/login.html.twig', [
+
+    ]);
+    }
+}
---
+++ src/Entity/User.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\UserRepository;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+/**
+ * @ORM\Table(name="app_user")
+ * @ORM\Entity(repositoryClass=UserRepository::class)
+ */
+class User implements UserInterface
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $username;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $mail;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $password;
+
+//not saved for the moment
+    private $roles;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getUsername(): ?string
+    {
+        return $this->username;
+    }
+
+    public function setUsername(string $username): self
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    public function getMail(): ?string
+    {
+        return $this->mail;
+    }
+
+    public function setMail(string $mail): self
+    {
+        $this->mail = $mail;
+
+        return $this;
+    }
+
+    public function getPassword(): ?string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getRoles()
+    {
+        return ["ROLE_USER"];
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function eraseCredentials()
+    {
+
+    }
+
+
+}
